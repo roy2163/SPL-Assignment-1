92bdab8102773b91b7a6be4be60e3df1600878f5
Omer Levi <omerax@post.bgu.ac.il>
2025-11-25 10:34:52 +0200

    Add files via upload

---
+++ SPL-Assignment-1/src/DJControllerService.cpp
@@ -0,0 +1,33 @@
+#include "DJControllerService.h"
+#include "MP3Track.h"
+#include "WAVTrack.h"
+#include <iostream>
+#include <memory>
+
+DJControllerService::DJControllerService(size_t cache_size)
+    : cache(cache_size) {}
+/**
+ * TODO: Implement loadTrackToCache method
+ */
+int DJControllerService::loadTrackToCache(AudioTrack& track) {
+    // Your implementation here 
+    return 0; // Placeholder
+}
+
+void DJControllerService::set_cache_size(size_t new_size) {
+    cache.set_capacity(new_size);
+}
+//implemented
+void DJControllerService::displayCacheStatus() const {
+    std::cout << "\n=== Cache Status ===\n";
+    cache.displayStatus();
+    std::cout << "====================\n";
+}
+
+/**
+ * TODO: Implement getTrackFromCache method
+ */
+AudioTrack* DJControllerService::getTrackFromCache(const std::string& track_title) {
+    // Your implementation here
+    return nullptr; // Placeholder
+}
